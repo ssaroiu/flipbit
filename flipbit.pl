bdf8905b54cbc129120fc1ac7df2c10c9093fd17
Stefan Saroiu <ssaroiu@microsoft.com>
2018-07-23 16:13:05 -0700

    in progress

---
+++ flipbit.pl
@@ -0,0 +1,73 @@
+#!/usr/bin/perl
+#
+# Initial script from StackExchange
+# https://unix.stackexchange.com/questions/196251/change-only-one-bit-in-a-file
+#
+# Arguments
+# [-r] [-B xxx -b xxx] -f <input_file>
+#
+
+
+use autodie;
+use strict;
+use utf8;
+use warnings qw(all);
+
+use Getopt::Long qw(:config no_ignore_case);
+use Pod::Usage qw(pod2usage);
+
+# VERSION
+
+=head1 SYNOPSIS
+
+    flipbit [-r] [-B <n> -b <n>] [file]
+
+=head1 OPTIONS
+
+    Options:
+
+    -r           flip random bit
+    -B           index of byte where bit will be flipped (starting from 0)
+    -b           index of bit within byte to flip (0-7)
+    -h           brief help
+    -v           help, usage, and examples
+
+  examples: 
+
+  ./flipbit -r <file>
+     prints the contents of <file> with one random bit flipped
+
+  ./flipbit -B 1 -b 3 <file>
+     prints the content of <file> with bit 11 (1 * 8 + 3) flipped
+ 
+=cut
+
+GetOptions(
+    q(r)                => \my $r,
+    q(Byte=i)           => \my $B,
+    q(bit=i)            => \my $b,
+    q(help)             => \my $help,
+    q(verbose)          => \my $verbose,
+) or pod2usage({ q(-verbose) => 1, q(-exitval) => 0 });
+
+pod2usage(q(-verbose) => 0) if $help;
+pod2usage(q(-verbose) => 1) if $verbose;
+
+# Byte and bit must either be both defined or both undefined
+if ((defined $B) ^ (defined $b)) {
+    pod2usage(1);
+}
+
+
+# If Byte (and bit) is undefined, r flag must be defined
+if (!(defined $B) && !(defined $r)) {
+    pod2usage(1);
+}
+
+# Actual code below
+
+my $file=<>; 
+
+substr($file,$B,1) = substr($file,$B,1) ^ chr(1<<$b); 
+
+print $file;
