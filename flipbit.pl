50bec9b412e0a85956e2b11f3077b9386706c5c3
Stefan Saroiu <ssaroiu@hotmail.com>
2018-07-23 21:40:38 -0700

    full first draft

--- flipbit.pl
+++ flipbit.pl
@@ -21,7 +21,7 @@ use Pod::Usage qw(pod2usage);
 
 =head1 SYNOPSIS
 
-    flipbit [-r -s] [-B <n> -b <n>] [file]
+    flipbit [-r -s] [-B <n> -b <n>] -f file
 
 =head1 OPTIONS
 
@@ -31,24 +31,26 @@ use Pod::Usage qw(pod2usage);
     -s           seed for the RNG when choosing the random bit
     -B           index of byte where bit will be flipped (starting from 0)
     -b           index of bit within byte to flip (0-7)
+    -f           name of file where bit will be flipped
     -h           brief help
     -v           help, usage, and examples
 
   examples: 
 
-  ./flipbit -r <file>
-     prints the contents of <file> with one random bit flipped
+  ./flipbit -r -f file
+     flip one random bit in <file>
 
-  ./flipbit -B 1 -b 3 <file>
-     prints the content of <file> with bit 11 (1 * 8 + 3) flipped
+  ./flipbit -B 1 -b 3 -f file
+     flip bit 11 (1 * 8 + 3) in <file>
  
 =cut
 
 GetOptions(
     q(r)                => \my $r,
-    q(seed)             => \my $s,
+    q(seed=i)           => \my $s,
     q(Byte=i)           => \my $B,
     q(bit=i)            => \my $b,
+    q(file=s)           => \my $file,
     q(help)             => \my $help,
     q(verbose)          => \my $verbose,
 ) or pod2usage({ q(-verbose) => 1, q(-exitval) => 0 });
@@ -56,34 +58,42 @@ GetOptions(
 pod2usage(q(-verbose) => 0) if $help;
 pod2usage(q(-verbose) => 1) if $verbose;
 
+# Check file is defined
+if (!defined $file) {
+    pod2usage(q(-verbose) => 0);
+}
+
 # Byte and bit must either be both defined or both undefined
 if ((defined $B) ^ (defined $b)) {
     pod2usage(1);
 }
-
 
 # If Byte (and bit) is undefined, r flag must be defined
 if (!(defined $B) && !(defined $r)) {
     pod2usage(1);
 }
 
-# Set the seed if defined
-if (defined $s) {
-    srand($s);
+# Set seed to 0 if undefined
+if (!defined $s) {
+    $s = 0;
+}
+print $s, "\n";
+srand($s);
+
+# If bit to flipped is random, pick a random byte and a random bit
+if (defined $r) {
+    my $fileSize = -s $file;
+    $B = int(rand($fileSize));
+    $b = int(rand(8));
 }
 
-open my $file, "<:raw", "$file";
+print $B, "\n";
+print $b, "\n";
 
-# if (defined $r) {
-#     my $fileSize = -s $file;
-#     my $offset = int(rand($fileSize));
-#     $B = int($offset  / 8);
-#     $b = $offset % 8;
-# #    print "$fileSize";
-#     die "done";
-# }
-
-#substr($file,$B,1) = substr($file,$B,1) ^ chr(1<<$b); 
-
-close $file;
-print $file;
+my $byteToFlip;
+open my $fh, '+<', $file                    or die "open failed; $!\n";
+sysread($fh, $byteToFlip, 1, $B) == 1       or die "read failed: $!\n";
+$byteToFlip = $byteToFlip ^ chr(1 << $b);
+seek($fh, 0, 0);
+syswrite($fh, $byteToFlip)                  or die "write failed: $!\n";
+close $fh                                   or die "close failed: $!\n";
